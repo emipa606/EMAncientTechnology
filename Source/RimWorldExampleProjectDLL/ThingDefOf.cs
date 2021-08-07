343e9fc4d989246e4993eb97198dc18f185483a6
emipa606 <emil.offermo@litet.net>
2021-08-07 23:26:57 +0200

    First publish

---
+++ Source/RimWorldExampleProjectDLL/ThingDefOf.cs
@@ -0,0 +1,13 @@
+﻿using RimWorld;
+using Verse;
+
+namespace EM_MM
+{
+    // Token: 0x02000003 RID: 3
+    [DefOf]
+    public static class ThingDefOf
+    {
+        // Token: 0x04000002 RID: 2
+        public static ThingDef EM_EyeofGodEffect;
+    }
+}
---
+++ Source/RimWorldExampleProjectDLL/Verb_EMSacrifice.cs
@@ -0,0 +1,46 @@
+﻿using RimWorld;
+using Verse;
+
+namespace EM_MM
+{
+    // Token: 0x02000002 RID: 2
+    public class Verb_EMSacrifice : Verb
+    {
+        // Token: 0x04000001 RID: 1
+        private const int DurationTicks = 600;
+
+        // Token: 0x06000001 RID: 1 RVA: 0x00002050 File Offset: 0x00000250
+        protected override bool TryCastShot()
+        {
+            bool result;
+            if (currentTarget.HasThing && currentTarget.Thing.Map != caster.Map)
+            {
+                result = false;
+            }
+            else
+            {
+                var powerBeam =
+                    (PowerBeam) GenSpawn.Spawn(ThingDefOf.EM_EyeofGodEffect, currentTarget.Cell, caster.Map);
+                powerBeam.duration = 600;
+                powerBeam.instigator = caster;
+                powerBeam.weaponDef = EquipmentSource?.def;
+                powerBeam.StartStrike();
+                if (EquipmentSource is {Destroyed: false})
+                {
+                    EquipmentSource.Destroy();
+                }
+
+                result = true;
+            }
+
+            return result;
+        }
+
+        // Token: 0x06000002 RID: 2 RVA: 0x0000212C File Offset: 0x0000032C
+        public override float HighlightFieldRadiusAroundTarget(out bool needLOSToCenter)
+        {
+            needLOSToCenter = false;
+            return 15f;
+        }
+    }
+}
